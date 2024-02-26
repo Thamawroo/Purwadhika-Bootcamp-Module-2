d481c37214d568865c1c0d0125dcc7a6c465f6b8
Thamawroo <hutamanugrah@gmail.com>
2024-02-26 21:56:49 +0700

    yes

---
+++ React-project/src/App.jsx
@@ -0,0 +1,21 @@
+import About from "./pages/about";
+import Home from "./pages/home";
+
+import styled from "styled-components";
+
+const StyledH1 = <styled className="h1"></styled>
+font-size: 32pageXOffset;
+
+
+function App() {
+ return (
+     <>
+     <h1>First React Project</h1>
+     <Home name="Budi" email="budi@gmail.com" />
+     <About />
+     </div>
+    </>
+  );
+}
+
+export default App;
